80c5ee4cbf29b5ca3bb63ee10b973924aedd1f6a
Scott Blomquist <scott@blomqui.st>
2025-11-30 22:04:02 -0800

    2025 day 2 part 2

--- 2025/02.cs
+++ 2025/02.cs
@@ -11,13 +11,28 @@ var line = lines.First();
 
 var nums = line.Extract<List<(long,long)>>(@"((\d+)-(\d+),?)+") ?? new List<(long, long)>();
 
-long c = 0;
+long c = 0, c2 = 0;
 
 foreach (var (min, max) in nums)
 {
     for (long i = min; i <= max; i++)
     {
         var s = i.ToString();
+        
+        for (int j = 1; j <= s.Length / 2; j++)
+        {
+            if (s.Length % j == 0)
+            {
+                var chunks = s.Chunk(j).Select(x => new string(x)).ToList();
+
+                if (chunks.Distinct().Count() == 1)
+                {
+                    c2 += i;
+                    break;
+                }
+            }
+        }
+
         if (s[0..(s.Length / 2)] == s[(s.Length / 2)..])
         {
             c += i;
@@ -26,3 +41,4 @@ foreach (var (min, max) in nums)
 }
 
 WriteLine(c);
+WriteLine(c2);
