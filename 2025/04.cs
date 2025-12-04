db20e124d69f3a21df2374f0a9e1f47db4243cc4
Scott Blomquist <scott@blomqui.st>
2025-12-03 21:16:20 -0800

    2025 day 4 part 2

--- 2025/04.cs
+++ 2025/04.cs
@@ -32,3 +32,42 @@ for (int i = 0; i < grid.Length; i++)
 }
 
 WriteLine(c);
+
+c = 0;
+var r = 1;
+
+while (r > 0)
+{
+    r = 0;
+    var newGrid = grid.Select(x => x.ToArray()).ToArray();
+    
+    for (int i = 0; i < grid.Length; i++)
+    {
+        for (int j = 0; j < grid[0].Length; j++)
+        {
+            var rolls = 0;
+            foreach (var (dx, dy) in dirs)
+            {
+                try
+                {
+                    if (grid[i + dy][j + dx] == '@') rolls++;
+                }
+                catch { }
+            }
+            if (rolls < 4 && grid[i][j] == '@')
+            {
+                c++;
+                r++;
+                newGrid[i][j] = '.';
+            }
+        }
+    }
+    grid = newGrid;
+}
+
+// for (int i = 0; i < grid.Length; i++)
+// {
+//     Console.WriteLine(string.Join("", grid[i]));
+// }
+
+WriteLine(c);
